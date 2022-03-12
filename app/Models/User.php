418e945c8810d94e89b0b32d9adab6ce1945e173
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2022-03-12 13:47:50 -0600

    Filament permissions

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,21 @@
 
 namespace App\Models;
 
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    public function canAccessFilament(): bool
+    {
+        return str_ends_with($this->email, 'emailme@joelohr.com') && $this->hasVerifiedEmail();
+    }
 
     /**
      * The attributes that are mass assignable.
